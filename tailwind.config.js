c2d9313ec7879b807a1e228abe3ed0b878d18d8f
blancojames88 <blancojames88@gmail.com>
2023-06-13 19:53:54 +0800

    added box sections

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,10 +6,14 @@ module.exports = {
       colors: {
         "custom-color": "#F1FCFD", // bg-custom-color
         "side-navbar": "#3D98AA", // bg-side-navbar
-        "side-navbar-active-text": "#3D98AA" // text-side-navbar-active-text
+        "side-navbar-active-text": "#3D98AA", // text-side-navbar-active-text
+        "search-bar": "#2EB9B9", // bg-search-bar
       },
       fontFamily: {
         Commissioner: ["Commissioner", "sans-serif"],
+      },
+      boxShadow: {
+        'custom': '0px 4px 20px rgba(0, 0, 0, 0.15)',
       },
     },
   },
