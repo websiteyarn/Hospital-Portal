7535f197bf7c9e8c9691345f5db7ea0782826bd2
ImperialDuck-dev <mjmuyco13@gmail.com>
2023-07-28 22:32:11 +0800

    done with user-profile

--- dist/user-profile.php
+++ dist/user-profile.php
@@ -22,16 +22,17 @@ session_start();
 <body class="bg-custom-color p-0 m-0 font-Commissioner flex-nowrap">
     <div class="flex">
        <!-- SIDEBAR NAV -->
-        <div class="sticky hidden lg:block lg:w-[172px] lg:h-screen bg-side-navbar rounded-tr-3xl rounded-br-3xl">
+       <div class="sticky hidden lg:block lg:w-[172px] lg:h-screen bg-side-navbar rounded-tr-3xl rounded-br-3xl">
             <!-- logo -->
+            <a href="health-board.php">
             <img src="../assets/logo.png" alt="logo" class="mx-auto pt-[34px]">
-
+            </a>
             <!-- nav -->
             <!-- HEALTH BOARD  -->
             <a href="health-board.php">
-                <div class="flex flex-col lg:w-[125px] lg:h-[144px] bg-white rounded-3xl mx-auto mt-[61px] justify-center items-center space-y-3 hover:scale-105 transform transition-transform duration-300">
-                    <img src="../assets/sidebar/health-board-active.png" alt="health-board-active">
-                    <h1 class="text-side-navbar-active-text">Health Board</h1> 
+                <div class="flex flex-col lg:w-[125px] lg:h-[144px] rounded-3xl mx-auto mt-[61px] justify-center items-center space-y-3 hover:scale-105 transform transition-transform duration-300">
+                    <img src="../assets/sidebar/health-board.png" alt="health-board-active">
+                    <h1 class="text-white">Health Board</h1> 
                 </div>
             </a>
 
@@ -61,9 +62,9 @@ session_start();
            
             <!-- FINANCE  -->
             <a href="user-finance.php">
-                <div class="flex flex-col lg:w-[125px] lg:h-[144px] rounded-3xl mx-auto justify-center items-center space-y-3 hover:scale-105 transform transition-transform duration-300">
-                    <img src="../assets/sidebar/finance.png" alt="finance">
-                    <h1 class="text-white">Finance</h1> 
+                <div class="flex flex-col lg:w-[125px] lg:h-[144px] bg-white rounded-3xl mx-auto justify-center items-center space-y-3 hover:scale-105 transform transition-transform duration-300">
+                    <img src="../assets/sidebar/finance-active.png" alt="finance">
+                    <h1 class="text-side-navbar-active-text">Finance</h1> 
                 </div>
             </a>    
         </div>
@@ -73,35 +74,27 @@ session_start();
             <!-- TOP ITEMS (USER-DROPDOWN) -->
             <div class="flex justify-end">
                 <!-- USER PROFILE -->
-                <div id="dropdown-button" class="mr-3 mt-6 z-50"> 
-                    <button class="flex flex-row lg:w-28 lg:h-12 bg-white justify-center rounded-3xl items-center"> 
-                        <img src="../assets/profilesample.jpg" alt="profile pic" class="rounded-full lg:w-10 lg:h-10"> 
-                        <img id="dropdown-arrow" src="../assets/arrow.png" alt="dropdown-arrow" class="ml-7 rotate-180">
-                    </button> 
-                    <!--profile dropdown-->                
-                    <ul id="dropdown-menu" class="absolute hidden w-40 right-3 mt-1"> 
-                        <li><a class="bg-white hover:bg-side-navbar py-2 px-4 block whitespace-no-wrap rounded-t-md" href="user-profile.php">Profile</a></li> 
-                        <li><a class="bg-white hover:bg-side-navbar py-2 px-4 block whitespace-no-wrap" href="user-change-pass.php">Change Password</a></li> 
+                <div id="dropdown-button" class=" mr-3 mt-6 z-50">
+                    <button class="flex flex-row lg:w-28 lg:h-12 bg-white justify-center rounded-3xl items-center">
+                        <img src="../assets/profilesample.jpg" alt="profile pic" class="rounded-full w-10 h-10">
+                        <img id="dropdown-arrow" src="../assets/arrow.png" alt="dropdown-arrow" class="hidden md:block md:mx-2 lg:ml-7 rotate-180">
+                    </button>
+                    <!--profile dropdown-->
+                    <ul id="dropdown-menu" class="absolute hidden w-40 right-3 mt-1">
+                        <li><a class="bg-white hover:bg-side-navbar py-2 px-4 block whitespace-no-wrap rounded-t-md"
+                                href="user-profile.php">Profile</a></li>
+                        <li><a class="bg-white hover:bg-side-navbar py-2 px-4 block whitespace-no-wrap"
+                                href="user-change-pass.php">Change Password</a></li>
                         <hr>
-                        <li><a class="bg-white hover:bg-side-navbar py-2 px-4 block whitespace-no-wrap rounded-b-md" href="splash.php"><?session_start();unset($_SESSION);
-                        session_destroy();session_write_close();header('Location: splash.php');die;?>Log out</a></li> 
+                        <li><a class="bg-white hover:bg-side-navbar py-2 px-4 block whitespace-no-wrap rounded-b-md"
+                                href="splash.php">
+                                <?session_start();unset($_SESSION);
+                        session_destroy();session_write_close();header('Location: splash.php');die;?>Log out
+                            </a></li>
                     </ul>
                 </div>
 
-                <!-- USER PROFILE MOBILE  -->
-                <div id="dropdown-button" class="lg:hidden mr-3 mt-6 rounded-lg"> 
-                    <button class=""> 
-                        <img src="../assets/profilesample.jpg" alt="profile pic" class="rounded-full w-7 h-7 lg:w-10 lg:h-10"> 
-                    </button> 
-                    <!-- profile dropdown -->
-                    <ul id="dropdown-menu" class="absolute hidden w-40 right-3 mt-1"> 
-                        <li><a class="bg-white hover:bg-side-navbar py-2 px-4 block whitespace-no-wrap rounded-t-md"href="user-profile.php">Profile</a></li> 
-                        <li><a class="bg-white hover:bg-side-navbar py-2 px-4 block whitespace-no-wrap" href="user-change-pass.php">Change Password</a></li> 
-                        <hr>
-                        <li><a class="bg-white hover:bg-side-navbar py-2 px-4 block whitespace-no-wrap rounded-b-md" href="splash.php"><?session_start();unset($_SESSION);
-                        session_destroy();session_write_close();header('Location: splash.php');die;?>Log out</a></li> 
-                    </ul>
-                </div>
+
             </div>
 
             <!-- MAIN CONTENT -->
@@ -111,7 +104,7 @@ session_start();
                 </div>
 
                 <!-- PROFILE BOX  -->
-                <div class="flex flex-col w-[1636px] h-[530px] mt-[45px] bg-white rounded-[30px] shadow-custom">
+                <div class="flex flex-col w-[90%] h-[830px] lg:w-[1636px] lg:h-[530px] mt-[45px] ml-5 bg-white rounded-[30px] shadow-custom">
 
                     <!-- The profile box includes the left items (profile, patient, name, birthdate, and sex) as well 
                     as the right items (email, mobile number, address, contact person, and contact mobile number).
@@ -124,22 +117,22 @@ session_start();
 
                         while($row = mysqli_fetch_assoc($result)){ ?>
 
-                    <div class="flex flex-row w-full h-full">
+                    <div class="flex flex-col lg:flex-row w-full h-fit">
 
                         <!-- LEFT ITEMS  -->
-                        <div class="w-[40%] h-full">
+                        <div class="w-fit lg:w-[40%] h-[40%] lg:h-full">
 
                             <!-- PROFILE PIC  -->
                             <div class="w-fit h-fit ml-10 z-50">
-                                <img src="../assets/profilesample.jpg" alt="user-profile" class="rounded-full lg:w-[203px] lg:h-[203px] -mt-8">
+                                <img src="../assets/profilesample.jpg" alt="user-profile" class="ml-[100px] rounded-full w-[90px] h-[90px] lg:ml-[0px] lg:w-[203px] lg:h-[203px] -mt-8">
                             </div>
 
                             <!-- PATIENT NUMBER  -->
                             <div class="w-full h-fit ml-10 mt-14">
-                                <span class="text-lg text-side-navbar-active-text">Patient Number</span>
+                                <span class="text-lg text-side-navbar-active-text">Patient Number: </span>
 
                                 <!-- PATIENT NUMBER VALUE -->
-                                <span class="text-lg font-medium ml-[160px]">
+                                <span class="text-lg font-medium lg:ml-[160px]">
                                     <?php 
                                         $len = strlen($row['userID']);
                                         if($len == 1){
@@ -156,71 +149,71 @@ session_start();
                             </div>
 
                             <!-- NAME  -->
-                            <div class="w-fit h-fit ml-10 mt-9">
-                                <span class="text-lg text-side-navbar-active-text">Name</span>
+                            <div class="w-fit h-fit ml-10 mt-3 lg:mt-9">
+                                <span class="lg:text-lg text-side-navbar-active-text">Name: </span>
 
                                 <!-- NAME VALUE -->
-                                <span class="text-lg font-medium ml-[244px]"><?php echo $row['first_name']." ".$row['last_name']; ?></span>
+                                <span class="text-lg font-medium lg:ml-[244px]"><?php echo $row['first_name']." ".$row['last_name']; ?></span>
                             </div>
 
                             <!-- DATE OF BIRTH  -->
-                            <div class="w-fit h-fit ml-10 mt-9">
-                                <span class="text-lg text-side-navbar-active-text">Date of Birth</span>
+                            <div class="w-fit h-fit ml-10 mt-3 lg:mt-9">
+                                <span class="text-lg text-side-navbar-active-text">Date of Birth: </span>
 
                                 <!-- DATE OF BIRTH VALUE -->
-                                <span class="text-lg font-medium ml-[184px]"><?php echo date("m-d-Y", strtotime($row['birth_date'])); ?></span>
+                                <span class="text-lg font-medium lg:ml-[184px]"><?php echo date("m-d-Y", strtotime($row['birth_date'])); ?></span>
                             </div>
 
                             <!-- SEX  -->
-                            <div class="w-fit h-fit ml-10 mt-9">
-                                <span class="text-lg text-side-navbar-active-text">Sex</span>
+                            <div class="w-fit h-fit ml-10 mt-3 lg:mt-9">
+                                <span class="text-lg text-side-navbar-active-text">Sex: </span>
 
                                 <!-- SEX VALUE -->
-                                <span class="text-lg font-medium ml-[260px]"><?php echo $row['gender']; ?></span>
+                                <span class="text-lg font-medium lg:ml-[260px]"><?php echo $row['gender']; ?></span>
                             </div>
                             
                         </div>
 
                         <!-- RIGHT ITEMS  -->
-                        <div class="w-[60%] h-full">
+                        <div class="w-fit lg:w-[60%] h-[40%] lg:h-full sm:mt-10 mt-2">
                             <!-- EMAIL -->
-                            <div class="w-fit h-fit ml-10 mt-14">
-                                <span class="text-lg text-side-navbar-active-text">Email</span>
+                            <div class="w-fit h-fit ml-10 mt-3 lg:mt-14">
+                                <span class="text-lg text-side-navbar-active-text">Email: </span>
 
                                 <!-- EMAIL VALUE -->
-                                <span class="text-lg font-medium ml-[200px]"><?php echo $row['email']; ?></span>
+                                <span class="text-lg font-medium lg:ml-[200px]"><?php echo $row['email']; ?></span>
                             </div>
 
                             <!-- MOBILE NUMBER  -->
-                            <div class="w-fit h-fit ml-10 mt-9">
+                            <div class="w-fit h-fit ml-10 mt-3 lg:mt-14">
                                 <span class="text-lg text-side-navbar-active-text">Mobile Number</span>
 
                                 <!-- MOBILE NUMBER VALUE -->
-                                <span class="text-lg font-medium ml-[118px]"><?php echo $row['contact_number']; ?></span>
+                                <span class="text-lg font-medium lg:ml-[118px]"><?php echo $row['contact_number']; ?></span>
                             </div>
 
                             <!-- ADDRESS  -->
-                            <div class="w-fit h-fit ml-10 mt-9">
+                            <div class="w-fit h-fit ml-10 mt-3 lg:mt-9">
                                 <span class="text-lg text-side-navbar-active-text">Address</span>
 
                                 <!-- ADDRESS VALUE -->
-                                <span class="text-lg font-medium ml-[180px]"><?php echo $row['address']; ?></span>
+                                <span class="text-lg font-medium lg:ml-[180px]"><?php echo $row['address']; ?></span>
                             </div>
 
                             <!-- CONTACT PERSON  -->
-                            <div class="w-fit h-fit ml-10 mt-9">
+                            <div class="w-fit h-fit ml-10 mt-3 lg:mt-14">
                                 <span class="text-lg text-side-navbar-active-text">Contact Person</span>
 
                                 <!-- CONTACT VALUE -->
-                                <span class="text-lg font-medium ml-[120px]"><?php echo $row['family']; ?></span>
+                                <span class="text-lg font-medium lg:ml-[120px]"><?php echo $row['family']; ?></span>
                             </div>
 
                             <!-- CONTACT MOBILE NUMBER  -->
-                            <div class="w-fit h-fit ml-10 mt-9">
+                            <div class="w-fit h-fit ml-10 mt-3 lg:mt-9">
                                 <span class="text-lg text-side-navbar-active-text">Contact Mobile No.</span>
 
                                 <!-- CONTACT MOBILE VALUE -->
-                                <span class="text-lg font-medium ml-[88px]"><?php echo $row['family_number']; ?></span>
+                                <span class="text-lg font-medium lg:ml-[88px]"><?php echo $row['family_number']; ?></span>
                             </div>
                         </div>
                     </div>
@@ -229,16 +222,19 @@ session_start();
                     ?>   
                     
                     <!-- BOTTOM ITEMS  -->
-                    <div class="flex w-fit h-20 ml-10">
-                        <img src="../assets/Rectangle-yellow.png" alt="honest-clause" class="w-1 h-10 mt-5">
-                        <span class="mt-7 ml-2 italic">I hereby certify that all the information provided are true and correct to the best of my knowledge.</span>
-                        
-                        <a href="user-profile-edit.php">
-                            <button class="flex w-[90px] h-[45px] mt-3 ml-[750px] justify-center items-center rounded-3xl shadow-custom hover:scale-105 transform transition-transform duration-300">
-                                <img src="../assets/edit-btn.png" alt="user-profile-edit">
-                                <span class="ml-1 text-gray-text text-lg">Edit</span>
-                            </button>
-                        </a>
+                    <div class="flex flex-col lg:flex-row lg:w-fit lg:h-20 ml-10 mt-2">
+                        <div class="flex flex-row w-fit">
+                            <img src="../assets/Rectangle-yellow.png" alt="honest-clause" class="w-1 h-10 mt-5">
+                            <span class="w-fit lg:mt-7 ml-2 italic">I hereby certify that all the information provided are true and correct to the best of my knowledge.</span>
+                        </div>
+                        <div class="flex justify-end mr-5">
+                            <a href="user-profile-edit.php">
+                                <button class="flex w-[90px] h-[45px] mt-3 lg:ml-[750px] justify-center items-center rounded-3xl shadow-custom hover:scale-105 transform transition-transform duration-300">
+                                    <img src="../assets/edit-btn.png" alt="user-profile-edit">
+                                    <span class="ml-1 text-gray-text text-lg">Edit</span>
+                                </button>
+                            </a>
+                        </div>
                     </div>
                 </div>
             </div>
